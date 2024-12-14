ed0dc09ca7e4bb27702b74a0e54b96ce00ec3be1
Andrada <andradaciuclea@yahoo.com>
2024-12-14 23:07:23 +0100

    update Second.java

--- src/day01_commit_push/Second.java
+++ src/day01_commit_push/Second.java
@@ -4,6 +4,7 @@ public class Second {
     public static void main(String[] args) {
 
         System.out.println("hello from second class");
+        System.out.println("More changes here");
     }
 
 }
